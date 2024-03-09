d618b15c919a3d4d137c2fa6a0ca171f6ab16bf3
gozdecanki <gozde_can.92@hotmail.com>
2024-03-09 17:54:22 +0300

    Hello world

---
+++ java-001-hello-world/src/main/java/com/gozdecanki/Java001HelloWorld.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.gozdecanki;
+
+/**
+ *
+ * @author Gözde
+ */
+public class Java001HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
