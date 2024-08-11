596129c19912812549e3b6697f80f8f86d8cb4ca
gozdecanki <gozde_can.92@hotmail.com>
2024-08-11 21:52:36 +0300

    Microservices
    Microservices

---
+++ java-041-microservice-project/day2/java-041-microservice-project/AuthService/src/main/java/com/gozdecanki/dto/response/DoRegisterResponseUsernameDto.java
@@ -0,0 +1,16 @@
+package com.gozdecanki.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder // bir sinifin kolayca nesne olusturmasini saglar
+@Data // set get metodlarını otomatik tanımlar
+@NoArgsConstructor // bos parametreli hazirlayici yapici metodu olusturur
+@AllArgsConstructor  // dolu parametreli hazirlayici yapici metodu olusturur
+public class DoRegisterResponseUsernameDto {
+
+    private String username;
+
+}
